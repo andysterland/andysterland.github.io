296a4a709cf4ddc404b5ad4d4583e25668622b67
Andy Sterland <andster@microsoft.com>
2016-03-06 15:21:58 -0800

    Adding console.time and takeHeapSnapshot demos.

---
+++ Demos/TracePoints/TracePoints.js
@@ -0,0 +1,32 @@
+(function () {
+    function isPrime(numberToTest) {
+        var testResult;
+        if (numberToTest == 1 || numberToTest == 2) {
+            testResult = true;
+        }
+
+        for (var i = numberToTest - 1; i > 1; i--) {
+            if (numberToTest % i == 0) {
+                // Ooops what a mistake to make
+                testResult = true;
+                break;
+            }
+        }
+
+        return testResult;
+    }
+
+    function getRandomWholeNumber(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    document.addEventListener("DOMContentLoaded", function(){
+        var traceThis = document.getElementById("traceThis");
+        traceThis.addEventListener("click", function () {
+            // Add console logging without modifying code
+            var randNum = getRandomWholeNumber(0, 100);
+
+            isPrime(randNum);
+        });
+    });
+})();
