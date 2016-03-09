8689a4a1969e1499df47674c9acaaa03951fa311
Andy Sterland <andster@microsoft.com>
2016-03-09 10:42:45 -0800

    Updating tracepoints demo

--- Demos/TracePoints/TracePoints.js
+++ Demos/TracePoints/TracePoints.js
@@ -1,15 +1,16 @@
 (function () {
     function isPrime(numberToTest) {
-        var testResult;
+        var testResult = true;
         if (numberToTest == 1 || numberToTest == 2) {
             testResult = true;
         }
 
+        // Loop that runs through every number except 1 and the test number
+        // As a modulus of 0 for this is expected... 
         for (var i = numberToTest - 1; i > 1; i--) {
+            // Ooops what a mistake to make
             if (numberToTest % i == 1) {
-                // Ooops what a mistake to make
-                testResult = true;
-                break;
+                testResult = false;
             }
         }
 
