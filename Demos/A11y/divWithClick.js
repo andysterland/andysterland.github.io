9553fb079dcdb4c9611991c5bc303f34b69c6bd0
Andy Sterland <andster@microsoft.com>
2016-03-15 20:16:23 -0700

    Updating demo sites for a11y.

--- Demos/A11y/divWithClick.js
+++ Demos/A11y/divWithClick.js
@@ -1,7 +1,16 @@
 (function(){
     
     window.addEventListener("DOMContentLoaded", function(){
-        
+        var clickMe = document.getElementById("clickMe");
+        clickMe.addEventListener("click", function(){
+           var out = document.getElementById("out");
+           out.textContent = "It was clicked!"; 
+        });
+        var clickMe2 = document.getElementById("clickMe2");
+        clickMe2.addEventListener("click", function(){
+           var out = document.getElementById("out");
+           out.textContent = "Thing 2 was clicked!"; 
+        });
     });    
     
 })();
