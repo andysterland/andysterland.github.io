9e8b6d29e64212c4705e5f0af53e715c79c9180a
Andy Sterland <andster@microsoft.com>
2016-08-18 15:52:40 -0700

    Missing domcontentloaded - doh

--- Demos/Console/consoleJSONObjects.js
+++ Demos/Console/consoleJSONObjects.js
@@ -1,31 +1,33 @@
 (function () {
-    var logValid = document.getElementById("logValid");
-    logValid.addEventListener("click", function () {
-        var x = {
-            a: "test",
-            b: 3,
-            c: [1, 2, 3],
-            d: false,
-            e: {
-                a: "hello"
-            },
-            f: null
-        };
-        console.log(x);
-    });
-    var logInvalid = document.getElementById("logInvalid");
-    logInvalid.addEventListener("click", function () {
-        var y = {
-            a: {
-                parent: a
-            },
-            b: new Date()
-        };
-        console.log(y);
-    });
-    var logElement = document.getElementById("logElement");
-    logElement.addEventListener("click", function () {
-        console.log(document.body);
+    window.addEventListener("DOMContentLoaded", function () {
+        var logValid = document.getElementById("logValid");
+        logValid.addEventListener("click", function () {
+            var x = {
+                a: "test",
+                b: 3,
+                c: [1, 2, 3],
+                d: false,
+                e: {
+                    a: "hello"
+                },
+                f: null
+            };
+            console.log(x);
+        });
+        var logInvalid = document.getElementById("logInvalid");
+        logInvalid.addEventListener("click", function () {
+            var y = {
+                a: {
+                    parent: a
+                },
+                b: new Date()
+            };
+            console.log(y);
+        });
+        var logElement = document.getElementById("logElement");
+        logElement.addEventListener("click", function () {
+            console.log(document.body);
+        });
     });
 }
 )();
