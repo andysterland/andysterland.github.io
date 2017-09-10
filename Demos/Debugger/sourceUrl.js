065d0449308500b57f9c3ec49fc3f049bf7501bb
Andy Sterland <andster@microsoft.com>
2017-09-09 18:30:36 -0700

    Fixing typo in syntax error page

--- Demos/Debugger/sourceUrl.js
+++ Demos/Debugger/sourceUrl.js
@@ -1,6 +1,7 @@
 function log(){
-    codeToEval = 'console.log("Hello from soureURL");\n\n';
-    codeToEval ='//# sourceURL=MyAwesomelibrary.js';
+    codeToEval = "";
+    codeToEval += 'console.log("Hello from soureURL");\n\n';
+    codeToEval +='//# sourceURL=MyAwesomelibrary.js';
     eval(codeToEval);
 }
 log();
